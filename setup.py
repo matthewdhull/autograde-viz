ac1e53b278791701fbf891aa52a190f26b4bfaef
Matthew Hull <matthewdhull@gmail.com>
2021-12-31 09:36:42 -0700

    add .toml files, testing on PyPI

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='autograde-viz',
-    version='0.0.1',    
-    description='D3 Autograding Utilities',
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autograde-viz",
+    version="0.0.1.2",
+    author="Matthew Hull",
+    author_email="matthewdhull@gmail.com.com",
+    description="D3 Autograding Utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/matthewdhull/autograde-viz',
-    author='Matthew Hull',
-    author_email='matthewdhull@gmail.com',
-    license='BSD 2-clause',
-    packages=['autograde-viz'],
-    install_requires=['pandas',
-                      'numpy',
-                      'selenium',                     
-                      ],
-
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
 )
+
--- src/autograde_viz/__init__.py
+++ src/autograde_viz/__init__.py
@@ -4,6 +4,6 @@ autograde-viz.
 A D3 Autograding-Utility.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.1.2"
 __author__ = 'Matthew Hull'
 __credits__ = 'Georgia Institute of Technology'
